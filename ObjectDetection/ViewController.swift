5cac9055d9e5bf2f3fca07ecb9b759fc50cef9e3
Vsevolod <11112365@mail.ru>
2019-11-20 10:52:39 +0300

    Initial Commit

---
+++ ObjectDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectDetection
+//
+//  Created by Всеволод Андрющенко on 20.11.2019.
+//  Copyright © 2019 Всеволод Андрющенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
